0a438fa048e6f80717cd74d040a282a36ac44bbf
Lucas Leal <lucas@pentaxialroot.com.br>
2018-12-20 14:05:12 -0200

    v1.0.0

--- cssvar.jquery.js
+++ cssvar.jquery.js
@@ -1,5 +1,5 @@
 /*
- *  jquery-cssvar-fallback - v0.0.1
+ *  jquery-cssvar-fallback - v1.0.0
  *  A jquery plugin to automatically parse css vars for browsers without support.
  *
  *  Made by Lucas Leal
@@ -8,23 +8,61 @@
 ;jQuery(function(){
     $.extend({
         cssVarFallback: function(options) {
+
+        	var getVars = function(str){
+        		var regex = /--([A-z0-9:\s-_]+);/gm;
+        		var m;
+        		var format = [];
+
+        		while ((m = regex.exec(str)) !== null) {
+        		    if (m.index === regex.lastIndex) {
+        		        regex.lastIndex++;
+        		    }
+        		    format.push( [ m[1].split(':')[0].trim() , m[1].split(':')[1].trim() ] );
+        		}
+        		return format;
+        	}
+
+        	var fix = function(file){
+				$.ajax({
+					method: "GET",
+					url: file
+				})
+				.done(function(str) {
+					var regex = '';
+					var vars = getVars(str);
+					for (var i = vars.length - 1; i >= 0; i--) {
+						regex = new RegExp('var\\(\\s*--'+vars[i][0]+'\\s*\\)',"gm");
+						str = str.replace(regex,vars[i][1]);
+					}
+					$('body').append('<style>'+str+'</style>');
+				})
+				.fail(function() {
+					console.log('cssVarFallback:: it was not possible to download css file');
+				});
+        	}
+
 			var defaults = {
 				file: ""
 			};
 			options = $.extend( {}, defaults, options );
-			$('body').append(`
-				<style id="cssvartemp">
-				:root{--vartest:100px;}
-				#cssvartest{display:none;width:0px;width:var(--vartest);}
-				</style>`);
+			$('body').append(
+				'<style id="cssvartemp">'+
+				':root{--vartest:100px;}'+
+				'#cssvartest{display:none;width:0px;width:var(--vartest);}'+
+				'</style>');
 			$('body').append('<div id="cssvartest">cssvartest</div>');
 			var width = $('#cssvartest').css('width');
 			$('#cssvartest').remove();
 			$('#cssvartemp').remove();
 			if (width=='100px'){
-				return true;
+				console.log('cssVarFallback:: browser already has support for var()');
 			}else{
-				return false;
+				if (options.file!==''){
+					fix(options.file);
+				}else{
+					console.log('cssVarFallback:: no input css file');
+				}
 			}
         }
     });
